cc0e6c2bd4e86e4f741b448c243ba51be10f0be2
fnfly2005 <fnfly2005@aliyun.com>
2019-03-18 14:57:19 +0800

    String类-特点

--- src/string/demo/StringDemo.java
+++ src/string/demo/StringDemo.java
@@ -2,9 +2,34 @@ package string.demo;
 
 public class StringDemo {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/*
+		 */
+		stringDemo1();
+		stringDemo2();
+	}
+
+	public static void stringDemo2() {
+		// TODO Auto-generated method stub
+		
+		String s = "abc";//	
+		String s1 = new String("abc");//
+		
+		System.out.println(s==s1);//false
+		
+		System.out.println(s.equals(s1));		
+	}
+
+	private static void stringDemo1() {
+		String s = "abc";
+		String s1 = "abc";		
+		System.out.println(s==s1);//true
+//		System.out.println("s="+s);
 	}
 
 }
