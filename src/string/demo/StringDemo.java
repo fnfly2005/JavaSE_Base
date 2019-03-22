af6642e9ffd867ef5be00015c9d75ab83ac54593
fnfly2005 <fnfly2005@aliyun.com>
2019-03-22 14:44:10 +0800

    优化注释&string类之获取一

--- src/string/demo/StringDemo.java
+++ src/string/demo/StringDemo.java
@@ -8,16 +8,27 @@ public class StringDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		/*
-		 */
-		stringDemo1();
-		stringDemo2();
+		
+
+		/*stringDemo1();
+		stringDemo2();*/
 		stringDemo3();
 
 	}
 
 
 	private static void stringDemo3() {
+		System.out.println("常用对象API-String类-常见功能-获取1");
+		String s = "中国abc12中";
+		System.out.println("长度："+ s.length());
+		System.out.println("首个位置："+ s.indexOf("中"));//返回查找到的第一个值的位置，-1为不存在
+		System.out.println("末个位置："+ s.lastIndexOf("中"));//返回查找到的最后一个值的位置，-1为不存在
+		System.out.println("位置字符："+ s.charAt(1));
+	}
+
+
+	private static void stringDemo2() {
+		System.out.println("常用对象API-String类-构造函数");
 		byte[] arr = {65,66,67,68};//字符编码创建字符串
 		char[] brr = {'S','B',' ','B','O','S','S'};//多字符创建字符串
 		String s1 = new String(brr,0,3);//字符串切片创建
@@ -25,22 +36,13 @@ public class StringDemo {
 	}
 
 
-	public static void stringDemo2() {
-		// TODO Auto-generated method stub
-		
-		String s = "abc";//	
-		String s1 = new String("abc");//
-		
-		System.out.println(s==s1);//false
-		
-		System.out.println(s.equals(s1));		
-	}
-
 	private static void stringDemo1() {
+		System.out.println("常用对象API-String类-特点");		
 		String s = "abc";
-		String s1 = "abc";		
-		System.out.println(s==s1);//true
-//		System.out.println("s="+s);
+		String s1 = new String("abc");//
+		String s2 = "abc";		
+		System.out.println(s==s1);//false
+		System.out.println(s==s2);//true
 	}
 
 }
