93d91954cef4fdfc9998bed6803a13d65315af04
fnfly2005 <fnfly2005@aliyun.com>
2019-04-26 15:07:48 +0800

    学习基本数据类型对象包装类-Integer

---
+++ src/wrapper/demo/WrapperDemo.java
@@ -0,0 +1,16 @@
+package wrapper.demo;
+
+public class WrapperDemo {
+
+	public static void main(String[] args) {
+		wrapperDemo1();
+	}
+
+	private static void wrapperDemo1() {
+		int num;
+		System.out.println("java-常用对象API-基本数据类型对象包装类-Integer");
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.toBinaryString(-6));
+	}
+
+}
