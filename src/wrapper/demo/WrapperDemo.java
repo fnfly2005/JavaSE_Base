c132832f7737c740bb78df2474235b302f90e2ee
fnfly2005 <fnfly2005@aliyun.com>
2019-05-09 18:44:28 +0800

    学习进制转换

--- src/wrapper/demo/WrapperDemo.java
+++ src/wrapper/demo/WrapperDemo.java
@@ -5,11 +5,20 @@ public class WrapperDemo {
 	public static void main(String[] args) {
 		wrapperDemo1();
 		wrapperDemo2();
+		wrapperDemo3();
+	}
+
+	private static void wrapperDemo3() {
+		System.out.println("java-常用对象API-基本数据类型对象包装类-进制转换");
+		System.out.println(Integer.toBinaryString(13));
+		System.out.println(Integer.toHexString(13));
+		System.out.println(Integer.toOctalString(13));//10进制转8进制
+		System.out.println(Integer.parseInt("3c", 16));//16进制转数字
 	}
 
 	private static void wrapperDemo2() {
 		System.out.println("java-常用对象API-基本数据类型对象包装类-字符串转成基本数值");
-		System.out.println(Integer.parseInt("123")+1);
+		System.out.println(Integer.parseInt("123")+1);//字符串转数字
 	}
 
 	private static void wrapperDemo1() {
