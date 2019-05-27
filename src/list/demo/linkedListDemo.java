f62dd77f3cf145a367de31c513eac48fbd939e29
fnfly2005 <fnfly2005@aliyun.com>
2019-05-27 15:11:06 +0800

    学习用linklist模拟堆栈及队列

--- src/list/demo/linkedListDemo.java
+++ src/list/demo/linkedListDemo.java
@@ -1,16 +1,56 @@
 package list.demo;
 
 import java.util.LinkedList;
+
+class DuiZhan{
+	private LinkedList link;
+	DuiZhan()
+	{		
+		link = new LinkedList();
+	}
+	
+	public void myAdd(Object obj)
+	{
+		link.addFirst(obj);
+	}
+	public Object myGet()
+	{
+		return link.pollFirst();
+	}
+	public boolean isNull()
+	{
+		return link.isEmpty();
+	}
+}
+
 public class linkedListDemo {
 
 	public static void main(String[] args) {
+		show1();
+		show2();
+		
+	}
+
+	private static void show2() {
+		System.out.println("常用对象API-集合框架-LinkedList集合练习-堆栈先进后出");
+		DuiZhan link = new DuiZhan();
+		link.myAdd("abc1");
+		link.myAdd("abc2");
+		link.myAdd("abc3");
+		link.myAdd("abc4");
+		while (!link.isNull())
+		{
+			System.out.println(link.myGet());
+		}
+	}
+
+	private static void show1() {
+		System.out.println("常用对象API-集合框架-LinkedList集合");
 		LinkedList link = new LinkedList();
 		link.addFirst("abc1");
 		link.addFirst("abc2");
 		link.addFirst("abc3");
 		link.addFirst("abc4");
-	
-		System.out.println("常用对象API-集合框架-LinkedList集合");
 		System.out.println(link.getFirst());//获取第一个但不删除
 		while (!link.isEmpty())
 		{
