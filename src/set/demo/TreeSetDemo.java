0e018fc6cb06873f8053373c664b37cd857ba7f4
fnfly2005 <fnfly2005@aliyun.com>
2019-06-04 14:44:36 +0800

    学习集合框架之泛型

--- src/set/demo/TreeSetDemo.java
+++ src/set/demo/TreeSetDemo.java
@@ -6,25 +6,21 @@ import java.util.TreeSet;
 
 import bean.test.Person;
 
-class TreeSetComparatorDemo implements Comparator
+class TreeSetComparatorDemo implements Comparator<Person>
 {
 
 	@Override
-	public int compare(Object o1, Object o2) {
-		Person p1 = (Person) o1;
-		Person p2 = (Person) o2;
+	public int compare(Person p1, Person p2) {
 		int temp = p1.getName().compareTo(p2.getName());
 		return temp==0?p1.getAge()-p2.getAge():temp;//该返回值应用于二叉树数据结构，为正放右边，为负放左边
 	}	
 }
 
-class TreeSetComparatorDemo2 implements Comparator
+class TreeSetComparatorDemo2 implements Comparator<String>
 {
 
 	@Override
-	public int compare(Object o1, Object o2) {
-		String s1 = (String) o1;
-		String s2 = (String) o2;
+	public int compare(String s1, String s2) {
 		int temp = s1.length()-s2.length();
 		return temp == 0?s1.compareTo(s2):temp;
 	}
@@ -44,18 +40,18 @@ public class TreeSetDemo {
 		 * 根据字符串长度进行排序
 		 */
 		System.out.println("java-常用对象API-集合框架-TreeSet集合练习-字符串长度排序");
-		TreeSet ts = new TreeSet(new TreeSetComparatorDemo2());
+		System.out.println("java-常用对象API-集合框架-泛型-概述");
+		TreeSet<String> ts = new TreeSet<String>(new TreeSetComparatorDemo2());
 		ts.add("aaaaa");
 		ts.add("zz");
 		ts.add("nbaq");
 		ts.add("cba");
 		ts.add("abc");
 		
-		Iterator it = ts.iterator();
+		Iterator<String> it = ts.iterator();
 		
 		while(it.hasNext()) {
-			String p = (String) it.next();
-			System.out.println(p);
+			System.out.println(it.next());
 		}
 	}
 
@@ -64,34 +60,34 @@ public class TreeSetDemo {
 		 * 不改变person类的情况下，实现按名称从小到大排列
 		 */
 		System.out.println("java-常用对象API-集合框架-TreeSet集合-Comparator比较器");
-		TreeSet ts = new TreeSet(new TreeSetComparatorDemo());
+		TreeSet<Person> ts = new TreeSet<Person>(new TreeSetComparatorDemo());
 		ts.add(new Person("zhangsan",28));
 		ts.add(new Person("wangwu",21));
 		ts.add(new Person("lisi",29));
 		ts.add(new Person("zhouqi",22));
 		ts.add(new Person("zhaoliu",25));
 		
-		Iterator it = ts.iterator();
+		Iterator<Person> it = ts.iterator();
 		
 		while(it.hasNext()) {
-			Person p = (Person) it.next();
+			Person p = it.next();
 			System.out.println(p.getName()+":"+p.getAge());
 		}
 	}
 
 	private static void show1() {
 		System.out.println("java-常用对象API-集合框架-TreeSet集合");
-		TreeSet ts = new TreeSet();
+		TreeSet<Person> ts = new TreeSet<Person>();
 		ts.add(new Person("zhangsan",28));
 		ts.add(new Person("wangwu",21));
 		ts.add(new Person("lisi",29));
 		ts.add(new Person("zhouqi",22));
 		ts.add(new Person("zhaoliu",25));
 		
-		Iterator it = ts.iterator();
+		Iterator<Person> it = ts.iterator();
 		
 		while(it.hasNext()) {
-			Person p = (Person) it.next();
+			Person p = it.next();
 			System.out.println(p.getName()+":"+p.getAge());
 		}
 	}
