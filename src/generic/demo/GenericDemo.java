82b0c3b4c5b77b848d51b671b1388c1b3c00f326
fnfly2005 <fnfly2005@aliyun.com>
2019-06-05 18:58:56 +0800

    学习集合框架之泛型方法

--- src/generic/demo/GenericDemo.java
+++ src/generic/demo/GenericDemo.java
@@ -6,11 +6,25 @@ public class GenericDemo {
 
 	public static void main(String[] args) {
 		
+		show1();
+		show2();
+		
+
+	}
+
+	private static void show2() {
+		System.out.println("java-常用对象API-集合框架-泛型-泛型方法");
+		Tool<String> tool = new Tool<String>();
+		tool.show(new Integer(4));
+		tool.print("abc");
+		
+	}
+
+	private static void show1() {
 		System.out.println("java-常用对象API-集合框架-泛型-泛型类");
 		Tool<Person> tool = new Tool<Person>();
 		tool.setObj(new Person());			
 		Person p = tool.getObj();
-
 	}
 
 }
--- src/generic/demo/Tool.java
+++ src/generic/demo/Tool.java
@@ -4,6 +4,7 @@ package generic.demo;
 public class Tool<o> {
 	/*
 	 * java-常用对象API-集合框架-泛型-泛型类
+	 * java-常用对象API-集合框架-泛型-泛型方法
 	 */
 	private o obj;
 
@@ -14,5 +15,12 @@ public class Tool<o> {
 	public void setObj(o obj) {
 		this.obj = obj;
 	}
+	
+	public static <w> void show(w str) {
+		System.out.println("show:" + str);//可以用方法上的泛型，另外静态方法不能访问类上定义的泛型
+	}
 
+	public void print(o str) {
+		System.out.println("print:" + str);//只能用对象上的泛型
+	}
 }
