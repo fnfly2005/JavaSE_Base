9f06f52ff1d3ccfa25c5d94faf30530e173c80d0
fnfly2005 <fnfly2005@aliyun.com>
2019-06-06 13:47:53 +0800

    学习集合框架之泛型接口

--- src/generic/demo/GenericDemo.java
+++ src/generic/demo/GenericDemo.java
@@ -8,8 +8,14 @@ public class GenericDemo {
 		
 		show1();
 		show2();
-		
+		show3();
 
+	}
+
+	private static void show3() {
+		System.out.println("java-常用对象API-集合框架-泛型-泛型接口");
+		InterDemo<String> id = new InterDemo<String>();
+		id.show("haha");		
 	}
 
 	private static void show2() {
@@ -28,3 +34,17 @@ public class GenericDemo {
 	}
 
 }
+
+interface Inter<T>{
+	public void show(T t);
+}
+
+class InterDemo<Q> implements Inter<Q>{
+
+	@Override
+	public void show(Q t) {
+		System.out.println(t);
+	}
+	
+}
+
