4d6360accda4de6b4ff2308415013cbf03fb5fa5
fnfly2005 <fnfly2005@aliyun.com>
2019-06-12 21:17:24 +0800

    Map集合练习-记录字母次数

---
+++ src/map/demo/MapTest.java
@@ -0,0 +1,49 @@
+package map.demo;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class MapTest {
+
+	public MapTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		/*
+		 * 需求 将字符串切分成字符并根据字母顺序打印其频数
+		 * 如：a(2)b(1).....;
+		 * 思路 将结果存入一个TreeMap集合
+		 * 打印该TreeMap
+		 */
+		System.out.println("java-常用对象API-集合框架-Map集合练习-记录字母次数代码");
+		String str = "fdgavcbsacdfs";
+		char [] c = str.toCharArray();
+		Map<Character,Integer> tm = new TreeMap<Character,Integer>();
+		
+		
+		for (int i=0;i<c.length;i++)
+		{
+			int count = 1;
+			char key = c[i];
+			Integer value = tm.get(key);
+			if(value != null)
+			{
+				count = value + 1;
+			}
+			tm.put(key, count);
+		}
+		
+		Set<Map.Entry<Character, Integer>> s = tm.entrySet();
+		Iterator<Map.Entry<Character, Integer>> it = s.iterator();
+		while(it.hasNext())
+		{
+			Map.Entry<Character, Integer> me = it.next();
+			System.out.print(me.getKey()+"("+me.getValue()+")");
+		}
+		
+	}
+
+}
