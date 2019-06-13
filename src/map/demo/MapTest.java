3008f6a5f9dbe5f859d9f9dc034a17cf0b3beab4
fnfly2005 <fnfly2005@aliyun.com>
2019-06-13 13:27:59 +0800

    练习Map查表法

--- src/map/demo/MapTest.java
+++ src/map/demo/MapTest.java
@@ -1,5 +1,6 @@
 package map.demo;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -12,6 +13,27 @@ public class MapTest {
 	}
 
 	public static void main(String[] args) {
+
+		show1();
+		show2("星期二");
+		
+	}
+
+
+
+	private static void show2(String s) {
+		Map<String,String> m = new HashMap<String,String>();
+		m.put("星期一", "Mon");
+		m.put("星期二", "Tus");
+		m.put("星期日", "Sun");
+		System.out.println("java-常用对象API-集合框架-Map集合练习-Map查表法");
+		if(m.containsKey(s))
+		{
+			System.out.println(m.get(s));
+		}
+	}
+
+	private static void show1() {
 		/*
 		 * 需求 将字符串切分成字符并根据字母顺序打印其频数
 		 * 如：a(2)b(1).....;
@@ -43,7 +65,7 @@ public class MapTest {
 			Map.Entry<Character, Integer> me = it.next();
 			System.out.print(me.getKey()+"("+me.getValue()+")");
 		}
-		
+		System.out.println();
 	}
 
 }
