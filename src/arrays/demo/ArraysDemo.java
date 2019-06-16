e49be9c3e9ef358d60057969c6213dd43dbeec35
fnfly2005 <fnfly2005@aliyun.com>
2019-06-16 14:57:09 +0800

    学习集合工具类Collection之toArray方法

--- src/arrays/demo/ArraysDemo.java
+++ src/arrays/demo/ArraysDemo.java
@@ -1,11 +1,32 @@
 package arrays.demo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ArraysDemo {
 
 	public static void main(String[] args) {
+		show1();
+		show2();
+		
+
+	}
+
+	private static void show2() {
+		System.out.println("java-常用对象API-集合框架-工具类-Collection-toArray方法");
+		List<String> list = new ArrayList<String>();
+		list.add("abc1");
+		list.add("abc2");
+		list.add("abc3");
+		
+		String[] arr = list.toArray(new String[list.size()]);
+		System.out.println(Arrays.toString(arr));
+		//转为数组后 不可以进行增删操作 相当于对集合进行了限定
+		
+	}
+
+	private static void show1() {
 		System.out.println("java-常用对象API-集合框架-工具类-Arrays-asList方法");
 		String[] arr = {"abc","haha","xixi"};
 		
@@ -20,8 +41,6 @@ public class ArraysDemo {
 		System.out.println(list2);
 		//如果数组的元素是对象 那会将对象分别存放到集合里
 		//如果数组的元素是基本类型 那会将数组作为集合中的元素进行存储
-		
-
 	}
 
 }
