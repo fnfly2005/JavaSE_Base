81ccb0dccb1a197226e53f861693c0e4105fab8b
fnfly2005 <fnfly2005@aliyun.com>
2019-06-21 13:02:12 +0800

    学习IO字符流缓冲区之复制文本文件

--- src/io/demo/CopyFileTest.java
+++ src/io/demo/CopyFileTest.java
@@ -1,5 +1,7 @@
 package io.demo;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -9,10 +11,48 @@ public class CopyFileTest {
 
 	private static final int BUFFER_SIZE = 1024;
 
-	public static void main(String[] args)  { //throws FileNotFoundException, IOException
+	public static void main(String[] args) throws IOException  { 
 		//copy1();
-		copy2();
+		//copy2();
+		System.out.println("java-IO流-字符流-缓冲区-复制文本文件");
+		FileReader fr = new FileReader("filewriter_demo.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		FileWriter fw = new FileWriter("copyfiletest1.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		copy3(br,bw);
+		copy4(br,bw);
+		bw.close();
+		br.close();
+	}
 
+
+
+	private static void copy4(BufferedReader br, BufferedWriter bw) throws IOException {
+		//可以对行进行操作
+		String line = null;
+		while((line=br.readLine())!= null)
+		{
+			bw.write(line);
+			bw.newLine();
+			bw.flush();
+		}
+
+	}
+
+
+
+	private static void copy3(BufferedReader br,BufferedWriter bw) throws IOException {
+		//单纯复制
+		int ch = 0;
+		
+		while ((ch=br.read())!=-1)
+		{
+			bw.write(ch);
+			bw.flush();
+		}
+
+		
 	}
 
 	private static void copy2() {
