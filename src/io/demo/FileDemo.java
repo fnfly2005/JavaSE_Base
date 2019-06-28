97ba3f173f93ed872045fcc4bed3ffe84997b8cc
fnfly2005 <fnfly2005@aliyun.com>
2019-06-28 19:57:15 +0800

    学习IO流-File对象-常见功能-获取

--- src/io/demo/FileDemo.java
+++ src/io/demo/FileDemo.java
@@ -1,11 +1,44 @@
 package io.demo;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.util.Date;
 
 public class FileDemo {
 
 	
 	public static void main(String[] args) {
+		show1();
+		show2();
+
+	}
+
+	
+	private static void show2() {
+		System.out.println("java-IO流-File对象-常见功能-获取");
+		File f1 = new File("copyfiletest1.txt");
+		String fn = f1.getName();
+		String apa = f1.getAbsolutePath();
+		String pa = f1.getPath();
+		String ppa = f1.getParent();
+		Long len = f1.length();
+		Long time = f1.lastModified();
+		
+		Date d = new Date(time);
+		
+		DateFormat df = DateFormat.getDateTimeInstance(DateFormat.LONG,DateFormat.LONG);
+		
+		String str_time = df.format(d);
+		
+		System.out.println("名称："+fn);
+		System.out.println("绝对路径："+apa);
+		System.out.println("相对路径："+pa);
+		System.out.println("父路径："+ppa);//需要指定对象绝对路径
+		System.out.println("修改时间："+str_time);
+		System.out.println("大小："+len);
+	}
+
+	private static void show1() {
 		System.out.println("java-IO流-File对象-构造函数&字段");
 		//可以将一个已存在的，或者不存在的文件或者目录封装成file对象
 		File f1 = new File("fannian/a.txt");
@@ -19,8 +52,6 @@ public class FileDemo {
 		System.out.println(f4);
 		System.out.println(f5);
 		System.out.println(File.pathSeparator);//路径分隔符，如环境变量路径
-		
-
 	}
 
 }
