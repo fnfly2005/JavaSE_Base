170984776d85ba6ab9f06342f3849b506c138865
fnfly2005 <fnfly2005@aliyun.com>
2019-07-01 20:20:23 +0800

    学习IO流-Properties集合-list方法

--- src/io/demo/PropertiesDemo.java
+++ src/io/demo/PropertiesDemo.java
@@ -5,18 +5,25 @@ import java.util.Properties;
 public class PropertiesDemo {
 
 	public static void main(String[] args) {
-		System.out.println("java-IO流-Properties集合的-基本功能");
+		
 		Properties prop = new Properties();
 		prop.setProperty("zhangsan", "30");
 		prop.setProperty("lisi", "31");
 		prop.setProperty("xiaoqiang", "20");
 		prop.setProperty("wangcai", "23");
 		
+		show1(prop);
+		System.out.println("java-IO流-Properties集合-list方法");
+		prop.list(System.out);
+
+	}
+
+	private static void show1(Properties prop) {
+		System.out.println("java-IO流-Properties集合-基本功能");
 		for (String sp:prop.stringPropertyNames())
 		{
 			System.out.println(sp+":"+prop.getProperty(sp));
 		}
-
 	}
 
 }
