ab730a2c90c7d4e9f71487be9b191673efb36f25
fnfly2005 <fnfly2005@aliyun.com>
2019-07-02 14:23:12 +0800

    学习IO流-Properties集合-store方法

--- src/io/demo/PropertiesDemo.java
+++ src/io/demo/PropertiesDemo.java
@@ -1,10 +1,11 @@
 package io.demo;
-
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		Properties prop = new Properties();
 		prop.setProperty("zhangsan", "30");
@@ -15,6 +16,10 @@ public class PropertiesDemo {
 		show1(prop);
 		System.out.println("java-IO流-Properties集合-list方法");
 		prop.list(System.out);
+		System.out.println("java-IO流-Properties集合-store方法");
+		FileOutputStream f = new FileOutputStream("FileDemo.txt");
+		prop.store(f, "info");//备注别写中文
+		f.close();
 
 	}
 
