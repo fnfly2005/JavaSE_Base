bfd862c5d6b8bdc04b90a33dc2793539e821fe7d
fnfly2005 <fnfly2005@aliyun.com>
2019-07-02 20:44:42 +0800

    学习IO流-Properties集合-修改配置信息

--- src/io/demo/PropertiesDemo.java
+++ src/io/demo/PropertiesDemo.java
@@ -1,7 +1,11 @@
 package io.demo;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
+
 
 public class PropertiesDemo {
 
@@ -14,13 +18,41 @@ public class PropertiesDemo {
 		prop.setProperty("wangcai", "23");
 		
 		show1(prop);
-		System.out.println("java-IO流-Properties集合-list方法");
-		prop.list(System.out);
+		show2(prop);
+		show3(prop);
+	
+		show4();
+
+	}
+
+	private static void show4() throws IOException {
+		System.out.println("java-IO流-Properties集合-修改配置信息");
+		Properties prop = new Properties();
+		File f = new File("PropertiesDemo.txt");
+		if(!f.exists())
+		{
+			f.createNewFile();
+		}
+		FileReader fr = new FileReader("PropertiesDemo.txt");
+		prop.load(fr);
+		prop.setProperty("zhangsan", "16");
+		fr.close();
+		
+		FileWriter fw = new FileWriter("PropertiesDemo.txt");
+		prop.store(fw, "newinfo");
+		fw.close();
+	}
+
+	private static void show3(Properties prop) throws IOException {
 		System.out.println("java-IO流-Properties集合-store方法");
-		FileOutputStream f = new FileOutputStream("FileDemo.txt");
+		FileOutputStream f = new FileOutputStream("PropertiesDemo.txt");
 		prop.store(f, "info");//备注别写中文
 		f.close();
+	}
 
+	private static void show2(Properties prop) {
+		System.out.println("java-IO流-Properties集合-list方法");
+		prop.list(System.out);
 	}
 
 	private static void show1(Properties prop) {
