09ba0287164093127eadb496ff749f127ec07499
fnfly2005 <fnfly2005@aliyun.com>
2019-07-04 14:18:31 +0800

    学习IO流-序列流-SequenceInputStream-枚举和迭代

--- src/io/demo/SequenceInputStreamDemo.java
+++ src/io/demo/SequenceInputStreamDemo.java
@@ -4,12 +4,44 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.SequenceInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Vector;
 
 public class SequenceInputStreamDemo {
 
 	public static void main(String[] args) throws IOException {
+		show1();
+		show2();
+
+	}
+
+	private static void show2() throws IOException {
+		System.out.println("java-IO流-序列流-SequenceInputStream-枚举和迭代");
+		ArrayList<FileInputStream> al = new ArrayList<FileInputStream>();
+		al.add(new FileInputStream("copyfiletest1.txt"));
+		al.add(new FileInputStream("FileDemo.txt"));
+		al.add(new FileInputStream("filewriter_demo.txt"));
+		Enumeration<FileInputStream> e = Collections.enumeration(al);
+		SequenceInputStream sis = new SequenceInputStream(e);
+		
+		FileOutputStream fos = new FileOutputStream("SequenceInputStreamDemo.txt");
+		
+		byte[] buf = new byte[1024];
+		
+		int len = 0;
+		while((len=sis.read(buf))!=-1)
+		{
+			fos.write(buf, 0, len);
+		}
+		
+		sis.close();
+		fos.close();
+		
+	}
+
+	private static void show1() throws IOException {
 		System.out.println("java-IO流-序列流-SequenceInputStream");
 		Vector<FileInputStream> v = new Vector<FileInputStream>();
 		
@@ -32,7 +64,6 @@ public class SequenceInputStreamDemo {
 		
 		sis.close();
 		fos.close();
-
 	}
 
 }
