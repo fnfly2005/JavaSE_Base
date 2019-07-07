425985f5fbad3ddfe5825826b374b218901fcdc5
fnfly2005 <fnfly2005@aliyun.com>
2019-07-07 19:23:07 +0800

    学习IO流-ObjectInputStream-对象的反序列化

--- src/io/demo/ObjectStreamDemo.java
+++ src/io/demo/ObjectStreamDemo.java
@@ -1,7 +1,9 @@
 package io.demo;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 
@@ -9,12 +11,25 @@ import bean.test.Person;
 
 public class ObjectStreamDemo {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		writeDemo();
+		readDemo();
+
+	}
+
+	private static void readDemo() throws IOException, ClassNotFoundException {
+		System.out.println("java-IO流-ObjectInputStream-对象的反序列化");
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("persondemo.object"));
+		Person p = (Person) ois.readObject();
+		ois.close();
+		System.out.println(p.getName()+":"+p.getAge());
+	}
+
+	private static void writeDemo() throws IOException {
 		System.out.println("java-IO流-ObjectOutputStream-对象的序列化");
 		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("persondemo.object"));
 		oos.writeObject(new Person("wangcai", 5));
 		oos.close();
-
 	}
 
 }
