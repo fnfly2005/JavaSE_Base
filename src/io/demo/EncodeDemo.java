bd9bdaf3b8ddaeec328de80ae494cf6946f37160
fnfly2005 <fnfly2005@aliyun.com>
2019-07-11 15:19:08 +0800

    学习IO流-按字节截取字符串

--- src/io/demo/EncodeDemo.java
+++ src/io/demo/EncodeDemo.java
@@ -2,13 +2,48 @@ package io.demo;
 
 import java.io.IOException;
 
+
 public class EncodeDemo {
 
 	public static void main(String[] args) throws IOException {
 		sample();
 		recode1();
 		recode2();
+		System.out.println("java-IO流-按字节截取字符串");
+		String st = "ab你好cd谢谢";
+		int len = st.getBytes("utf-8").length;
+		for (int x =0;x<len;x++) {
+			System.out.println("截取"+(x+1)+"个字节结果是："+cutStringByByte(st,x+1));
+		}
+		
 	}
+
+
+
+	private static String cutStringByByte(String st, int i) throws IOException {
+		byte[] buf = st.getBytes("utf-8");
+		int count = 0;
+		for(int x=i-1;x>=0;x--) {
+			if(buf[x]<0) {
+				count++;
+			}
+			else {
+				break;
+			}
+		}
+		if(count%3==1) {
+			return new String(buf, 0, i-1,"utf-8");
+		}
+		else if (count%3==2) {
+			return new String(buf, 0, i-2,"utf-8");
+		}
+		else {
+			return new String(buf, 0, i,"utf-8");
+		}
+		
+	}
+
+
 
 	private static void recode2() throws IOException {
 		System.out.println("java-IO流-编码解码问题二");
