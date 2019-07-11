ad168fd32816f67cf335e1f3fea38f416fa288d0
fnfly2005 <fnfly2005@aliyun.com>
2019-07-11 12:41:01 +0800

    学习IO流-编码解码问题

--- src/io/demo/EncodeDemo.java
+++ src/io/demo/EncodeDemo.java
@@ -6,7 +6,30 @@ public class EncodeDemo {
 
 	public static void main(String[] args) throws IOException {
 		sample();
+		recode1();
+		recode2();
+	}
 
+	private static void recode2() throws IOException {
+		System.out.println("java-IO流-编码解码问题二");
+		String s = "你好!";		
+		byte[] buf = s.getBytes("gbk");
+		
+		String str = new String(buf,"UTF-8");
+		System.out.println(str);
+		byte[] bu2 = str.getBytes("UTF-8");
+		System.out.println(new String(bu2,"gbk"));
+	}
+
+	private static void recode1() throws IOException {
+		System.out.println("java-IO流-编码解码问题一");
+		String s = "你好!";		
+		byte[] buf = s.getBytes("gbk");
+		
+		String str = new String(buf,"ISO8859-1");
+		System.out.println(str);
+		byte[] bu2 = str.getBytes("ISO8859-1");
+		System.out.println(new String(bu2,"gbk"));
 	}
 
 	private static void sample() throws IOException {
