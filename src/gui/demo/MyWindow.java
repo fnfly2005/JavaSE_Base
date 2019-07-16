c4ef7cf8ccbf16ce69fc8d348e3efaca775db7b5
fnfly2005 <fnfly2005@aliyun.com>
2019-07-16 19:44:49 +0800

    练习-GUI-列出目录内容

---
+++ src/gui/demo/MyWindow.java
@@ -0,0 +1,77 @@
+package gui.demo;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
+
+public class MyWindow extends JFrame {
+
+	protected static final String LINE_SEPARATOR = System.getProperty("line.separator");
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		System.out.println("java-GUI-练习-列出目录内容");
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MyWindow frame = new MyWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MyWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(6, 24, 315, 30);
+		contentPane.add(textField);
+		textField.setColumns(20);
+
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(6, 66, 438, 206);
+		contentPane.add(textArea);
+		
+		JButton button = new JButton("转到");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.setText("");
+				String textdir = textField.getText();
+				File dir = new File(textdir);
+				if(dir.exists()) {
+					String[] list = dir.list();
+					for (String s:list) {
+						textArea.append(s+LINE_SEPARATOR);
+					}
+				}
+			}
+		});
+		button.setBounds(332, 24, 112, 30);
+		contentPane.add(button);
+		
+
+	}
+}
